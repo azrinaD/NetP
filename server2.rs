250bafc9e0b0b8afa4c685b07738f896cb4fd12c
azrinaD <48429062+azrinaD@users.noreply.github.com>
2019-06-12 01:52:52 +0800

    Update server2.rs

--- server2.rs
+++ server2.rs
@@ -1,38 +1,49 @@
-const http = require('http')
-const port = 3000
+// Updated example from http://rosettacode.org/wiki/Hello_world/Web_server#Rust
+// to work with Rust 1.0 beta
 
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
+use std::net::{TcpStream, TcpListener};
+use std::io::{Read, Write};
+use std::thread;
+
+
+fn handle_read(mut stream: &TcpStream) {
+    let mut buf = [0u8 ;4096];
+    match stream.read(&mut buf) {
+        Ok(_) => {
+            let req_str = String::from_utf8_lossy(&buf);
+            println!("{}", req_str);
+            },
+        Err(e) => println!("Unable to read stream: {}", e),
+    }
 }
 
-const server = http.createServer(requestHandler)
+fn handle_write(mut stream: TcpStream) {
+    let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Hello world</body></html>\r\n";
+    match stream.write(response) {
+        Ok(_) => println!("Response sent"),
+        Err(e) => println!("Failed sending response: {}", e),
+    }
+}
 
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
-#[macro_use]
-extern crate log;
-extern crate env_logger;
-
-extern crate simple_server;
-
-use simple_server::Server;
+fn handle_client(stream: TcpStream) {
+    handle_read(&stream);
+    handle_write(stream);
+}
 
 fn main() {
-    env_logger::init().unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Listening for connections on port {}", 8080);
 
-    let host = "127.0.0.1";
-    let port = "7878";
-
-    let server = Server::new(|request, mut response| {
-        info!("Request received. {} {}", request.method(), request.uri());
-        Ok(response.body("Hello Rust!".as_bytes())?)
-    });
-
-    server.listen(host, port);
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(|| {
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("Unable to connect: {}", e);
+            }
+        }
+    }
 }
