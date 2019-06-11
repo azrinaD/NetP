7db8654dac7bcd1a3ebfd08eed1615ef30cff26a
azrinaD <48429062+azrinaD@users.noreply.github.com>
2019-06-12 01:01:22 +0800

    Update server2.rs

--- server2.rs
+++ server2.rs
@@ -1,3 +1,20 @@
+const http = require('http')
+const port = 3000
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
 #[macro_use]
 extern crate log;
 extern crate env_logger;
